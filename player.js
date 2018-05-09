079c85174fe53bbb8f6f6b2677737b5030cdc2af
BrianArchibald <brianallenarchibald@gmail.com>
2018-05-08 20:34:28 -0500

    added pre, next, time bar, posters

--- player.js
+++ player.js
@@ -5,6 +5,8 @@ var poster = ['Poster1.jpg','Poster2.jpg','Poster3.jpg',];
 var songTitle = document.getElementById('songTitle');
 var fillBar = document.getElementById('fill');
 
+var currentTime = document.getElementById('currentTime');
+
 //create an object for audio class
 
 var song = new Audio();
@@ -30,5 +32,40 @@ function playOrPauseSong() {
 
 song.addEventListener('timeupdate', function() {
 	var position = song.currentTime / song.duration;
-	fillBar.style.width
-})
+	fillBar.style.width = position + 100 +'%';
+
+	converTime(Math.round(song.currentTime));
+});
+
+function convertTime(seconds) {
+	var min = Math.floor(seconds / 60);
+	var sec = seconds % 60;
+
+	min = (min < 10) ? '0' + min : min;
+	sec = (sec < 10) ? '0' + sec : sec;
+	currentTime.textContent = min + ':' + sec;
+
+	totalTime(Math.round(song.duration));
+}
+
+function next() {
+	currentSong++;
+	if(currentSong > 2) {
+		currentSong = 0;
+	}
+	playSong();
+	$('#play img').attr('src',"Pause.png");
+	$('#image img').attr('src',poster[currentSong]);
+	$('#bg img').attr('src',poster[currentSong]);
+}
+
+function pre() {
+	currentSong--;
+	if(currentSong < 0) {
+		currentSong = 2;
+	}
+	playSong();
+	$('#play img').attr('src',"Pause.png");
+	$('#image img').attr('src',poster[currentSong]);
+	$('#bg img').attr('src',poster[currentSong]);
+}
